ff57f0610fb5c97d6c409e1fae305ec5886c6fa7
Mayur Agarwal <mr.mayurrrr@gmail.com>
2020-11-02 13:48:48 +0530

    Revert "Progress3"

--- build/app/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
+++ build/app/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
@@ -22,17 +22,17 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int common_google_signin_btn_text_dark = 0x7f020002;
-        public static final int common_google_signin_btn_text_dark_default = 0x7f020003;
-        public static final int common_google_signin_btn_text_dark_disabled = 0x7f020004;
-        public static final int common_google_signin_btn_text_dark_focused = 0x7f020005;
-        public static final int common_google_signin_btn_text_dark_pressed = 0x7f020006;
-        public static final int common_google_signin_btn_text_light = 0x7f020007;
-        public static final int common_google_signin_btn_text_light_default = 0x7f020008;
-        public static final int common_google_signin_btn_text_light_disabled = 0x7f020009;
-        public static final int common_google_signin_btn_text_light_focused = 0x7f02000a;
-        public static final int common_google_signin_btn_text_light_pressed = 0x7f02000b;
-        public static final int common_google_signin_btn_tint = 0x7f02000c;
+        public static final int common_google_signin_btn_text_dark = 0x7f020000;
+        public static final int common_google_signin_btn_text_dark_default = 0x7f020001;
+        public static final int common_google_signin_btn_text_dark_disabled = 0x7f020002;
+        public static final int common_google_signin_btn_text_dark_focused = 0x7f020003;
+        public static final int common_google_signin_btn_text_dark_pressed = 0x7f020004;
+        public static final int common_google_signin_btn_text_light = 0x7f020005;
+        public static final int common_google_signin_btn_text_light_default = 0x7f020006;
+        public static final int common_google_signin_btn_text_light_disabled = 0x7f020007;
+        public static final int common_google_signin_btn_text_light_focused = 0x7f020008;
+        public static final int common_google_signin_btn_text_light_pressed = 0x7f020009;
+        public static final int common_google_signin_btn_tint = 0x7f02000a;
     }
     public static final class drawable {
         private drawable() {}
--- lib1/screens/home/bloc/home_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:notado/packages/packages.dart';
-
-abstract class HomeState extends Equatable {
-  const HomeState();
-  @override
-  List<Object> get props => [];
-}
-
-
